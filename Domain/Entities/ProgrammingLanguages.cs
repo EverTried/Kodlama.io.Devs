052f534648a7046befb808f25679e30c8aa2da2a
Hüseyin Caner İBRAHİMOĞLU <huseyincaner.ibrahimoglu@ogr.iu.edu.tr>
2022-09-01 23:56:08 +0300

    Add project files.

---
+++ Domain/Entities/ProgrammingLanguages.cs
@@ -0,0 +1,24 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ProgrammingLanguages:Entity
+    {
+        public string LanguageName { get; set; }
+
+        public ProgrammingLanguages()
+        {
+        }
+
+        public ProgrammingLanguages(int id,string languageName):this()
+        {
+            Id = id;
+            LanguageName = languageName;
+        }
+    }
+}
---
+++ Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Domain.Entities;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+
+        public DbSet<ProgrammingLanguages> ProgrammingLanguages { get; set; }
+
+        public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if (!optionsBuilder.IsConfigured)
+            //    base.OnConfiguring(
+            //        optionsBuilder.UseSqlServer(Configuration.GetConnectionString("SomeConnectionString")));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProgrammingLanguages>(a =>
+            {
+                a.ToTable("Languages").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.LanguageName).HasColumnName("Name");
+            });
+
+
+
+            ProgrammingLanguages[] programmingLanguagesEntitySeeds = { new(1, "C#"), new(2, "C++") };
+            modelBuilder.Entity<ProgrammingLanguages>().HasData(programmingLanguagesEntitySeeds);
+
+
+        }
+    }
+}
